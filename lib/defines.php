8f5c564984e79e3116808673fd6fe1d0bcd3796d
Doomsta <doomsta2k7@gmx.de>
2013-06-18 22:20:18 +0200

    add $_gearSlot_name to defines

--- lib/defines.php
+++ lib/defines.php
@@ -101,6 +101,31 @@ $_socket_Color = array(
 	8 => 'Blue'
 );
 
+# http://images3.wikia.nocookie.net/__cb20090410062624/wowwiki/images/a/ae/InventorySlots.jpg
+$_gearSlot_name => array(
+			1 => 'head', 
+			2 => 'neck'
+			3 => 'shoulder',
+			15 => 'back',
+			5 => 'chest',
+			4 => 'shirt',
+			19 => 'tabard',
+			9 => 'wrist',
+			
+			10 => 'hands',
+			6 => 'waist', //belt
+			7 => 'legs',
+			8 => 'feet',
+			11 => 'finger1',
+			12 => 'finger2',
+			13 => 'trinket1',
+			14 => 'trinket2',
+			
+			16 => 'mainHand',
+			17 => 'offHand',
+			18 => 'relik'
+);
+
 # stat->id to stat->typ
 # http://collab.kpsn.org/display/tc/Item+template+tc2#Itemtemplatetc2-stat_type
 $_stat_typ = array(
