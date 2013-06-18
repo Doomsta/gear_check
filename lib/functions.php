a5ece2b7ac2e11eb0bfc374c80a46de9ba2683dd
Doomsta <doomsta2k7@gmx.de>
2013-06-18 23:56:37 +0200

    improve get_item_stats($id)

--- lib/functions.php
+++ lib/functions.php
@@ -2,27 +2,21 @@
 function get_item_stats($id)
 {
 	include('defines.php'); // >.<
-	$query = 'SELECT `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`,  
+	$query = 'SELECT `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`,  
 				`stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`,  
 				`stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`,  
 				`stat_type8`, `stat_value8` 
 			FROM `item_template` 
 			WHERE `entry` = \''.$id.'\'';
 	$result = mysql_query($query);
-	$data = mysql_fetch_array($result);
-	for($i=0;$i<count($data);$i++)
-		unset($data[$i]);
-	for($i=1;$i<9;$i++) 
+	$tmp = mysql_fetch_array($result);
+	$data = array();
+	for($i=1;$i<9;$i++)
 	{
-		if($data['stat_type'.$i] == 0)
-		{
-			unset($data['stat_type'.$i]);
-			unset($data['stat_value'.$i]);
-		}
-		else
-			$data['stat_type'.$i] = $_stat_typ[$data['stat_type'.$i]];
+		if($tmp['stat_value'.$i] == 0)
+			break;
+		$data[$_stat_typ[$tmp['stat_type'.$i]]] = $tmp['stat_value'.$i];
 	}
-	
 	return($data);
 }
 function add_item_gems($item)
